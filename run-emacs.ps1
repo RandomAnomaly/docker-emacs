925809511ef0058a6988a4812d606fe1678bb6f8
Jack Gammon <jack.gammon@trademe.co.nz>
2020-01-24 11:10:16 +1300

    feat: Add directories to mount

--- run-emacs.ps1
+++ run-emacs.ps1
@@ -1,5 +1,5 @@
 $docker_build_command = 'docker build . --tag docker-emacs'
-$docker_run_command = "docker run --rm -e DISPLAY=host.docker.internal:0.0 --volume=$pwd/emacs-config:/home/emacs-user/emacs-config-live-edit -ti docker-emacs"
+$docker_run_command = "docker run --rm -e DISPLAY=host.docker.internal:0.0 --volume=$pwd/emacs-config:/home/emacs-user/emacs-config-live-edit --volume=$pwd/working-directory:/home/emacs-user/source --volume=$pwd/emacs-config/.emacs.d/elpa:/home/emacs-user/.emacs.d/elpa -ti docker-emacs"
 
 
 echo $docker_run_command
