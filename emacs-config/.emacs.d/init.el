79108407df34d4a30c4753d11b0b2b51ea0395ef
Jack Gammon <jack.gammon@trademe.co.nz>
2020-01-24 14:45:55 +1300

    feat: Ensure install happens on first start

--- emacs-config/.emacs.d/init.el
+++ emacs-config/.emacs.d/init.el
@@ -9,3 +9,17 @@
 (org-babel-load-file
  (expand-file-name "Settings.org"
 		   "~/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck sublimity yasnippet which-key use-package telephone-line spacemacs-theme projectile helm eyebrowse dumb-jump))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
